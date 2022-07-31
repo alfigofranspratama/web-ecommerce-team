4769a8c5ca33caffa237c050730c74dd49cd06cf
京 Ryugen <109148993+ryugenxd@users.noreply.github.com>
2022-08-01 01:14:43 +0800

    update index.php

--- admin/index.php
+++ admin/index.php
@@ -9,7 +9,7 @@ $DATA = $Ryugen -> query("SELECT * FROM ".TABEL);
 <html lang="id">
     <head>
     	 <meta charset="UTF-8">
-         <meta name="description" content="INDEX ADMINISTRATOR">
+         <meta name="description" content="Ini halaman INDEX ADMINISTRATOR bang">
     	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-calable=no"/>
         <title>Page Administrator</title>
         <link rel="stylesheet" href="../asset/bootstrap/css/bootstrap.min.css"/>
