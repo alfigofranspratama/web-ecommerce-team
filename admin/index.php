217dc53ae7da71768a86332b31497ca3a0a09fa5
京 Ryugen <109148993+ryugenxd@users.noreply.github.com>
2022-08-01 00:36:53 +0800

    update index.php

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
     	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-calable=no"/>
-        <title></title>
+        <title>Page Administrator</title>
      </head>
     <body>
     </body>
