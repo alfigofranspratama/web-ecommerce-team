28da833351c9d12c633e989db6e5ff92bb632f43
京 Ryugen <109148993+ryugenxd@users.noreply.github.com>
2022-08-01 07:07:18 +0800

    update connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -26,19 +26,6 @@ class Database {
 }
 
 
-/*
-$connect = mysqli_connect(HOST,USER,PASS,DB);
-
-function __Katakunci($data){
-	global $connect;
-	$query = mysqli_query($connect,$data);
-	$gwggeh = [];
-	while($ryu = mysqli_fetch_assoc($query)){
-		$gwggeh[]=$ryu;
-	}
-	return $gwggeh;
-}
-*/
 /*************************
 GW GG XD RUYUGEN XD
 **************************/
