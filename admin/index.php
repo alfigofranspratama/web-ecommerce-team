270120538a10717fe6596773b605e05d79869695
京 Ryugen <109148993+ryugenxd@users.noreply.github.com>
2022-08-01 00:37:28 +0800

    update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,7 @@
     <head>
     	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-calable=no"/>
         <title>Page Administrator</title>
+        <link rel="stylesheet" href="./bootstrap-5.2.0-dist/css/bootstrap.min.css"/>
      </head>
     <body>
     </body>
